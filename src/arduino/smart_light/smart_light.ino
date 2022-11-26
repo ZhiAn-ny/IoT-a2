156664d5c660f4b6bc92807928a19b273f59fff2
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2022-11-26 18:46:43 +0100

    Add blink task (WIP)

--- src/arduino/smart_light/smart_light.ino
+++ src/arduino/smart_light/smart_light.ino
@@ -2,6 +2,10 @@
 #include "Scheduler.h"
 #include "SmartLightTask.h"
 
+#include "tasks/BlinkTask.h"
+
+using namespace bridge_scheduling;
+
 Scheduler sched;
 
 
@@ -10,13 +14,22 @@ void setup(){
   Serial.begin(9600);
   Serial.println("Welcome to Smart Bridge Project");
 
-
   sched.init(100);
+  Serial.println("Scheduler initialized");
  
-  Task* t0 = new SmartLightTask();
-  t0->init(100);
+  Task* test = new tasks::BlinkTask(LED_PIN_B);
+  test->init(1000);
+  Serial.println("Task created");
+  sched.addTask(test);
 
-  sched.addTask(t0);
+  Serial.println("Ready");
+
+  // Task* t0 = new SmartLightTask();
+  // t0->init(100);
+  // Serial.println("Task initialized");
+
+  // sched.addTask(t0);
+  // Serial.println("Task added to scheduler");
    
 }
 
---
+++ src/arduino/smart_light/tasks/BlinkTask.cpp
@@ -0,0 +1,26 @@
+#include "BlinkTask.h"
+
+using namespace bridge_scheduling::tasks;
+
+BlinkTask::BlinkTask(int pin)
+{
+    this->pin_ = pin;
+}
+
+void BlinkTask::init(int period)
+{
+    Task::init(period);
+    this->led_ = new Led(this->pin_);
+    this->state_ = OFF;
+}
+
+void BlinkTask::tick()
+{
+    if (this->state_ == ON) {
+        this->led_->switchOff();
+        this->state_ = OFF;
+    } else {
+        this->led_->switchOn();
+        this->state_ = ON;
+    }
+}
