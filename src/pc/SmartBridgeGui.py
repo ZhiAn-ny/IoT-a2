49b5b0d81614c957590d936d190e96298957fe9e
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2022-12-08 14:39:55 +0100

    Move plotter into main class
    Cause: difficulties in dynamic update of plot

--- src/pc/SmartBridgeGui.py
+++ src/pc/SmartBridgeGui.py
@@ -1,18 +1,45 @@
 # Imports
+from matplotlib import animation, figure, style
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import tkinter as tk
 import threading as th
+import numpy as np
 import time
-from tkinter import *
 
-from plotter.Plotter import Plotter
 from board.ArduinoBoard import ArduinoBoard
 
-class SmartBridgeGui:
-    __win: Tk = Tk()
-    __plot: Plotter = Plotter()
-    __uno: ArduinoBoard = ArduinoBoard()
+style.use("ggplot")
 
-    def __show_plot(self):
-        self.__plot.add_plot(self.__win)
+class SmartBridgeGui:
+    __win: tk.Tk = tk.Tk()
+    __fig: Figure = None
+    __plot = None
+    __uno: ArduinoBoard = ArduinoBoard()
+    __anm = None
+
+    def __animate(self, i):
+        pullData = open('src\pc\plotter\WaterSampling.txt','r').read()
+        dataArray = pullData.split('\n')
+        xar=[]
+        yar=[]
+        for eachLine in dataArray:
+            if len(eachLine)>1:
+                x,y = eachLine.split(',')
+                xar.append(int(x))
+                yar.append(int(y))
+        self.__plot.clear()
+        self.__plot.plot(xar,yar)
+
+    def __add_plotter(self):
+        self.__fig = Figure(figsize=(5,4), dpi=100)
+        self.__plot = self.__fig.add_subplot(111)
+
+        canvas = FigureCanvasTkAgg(self.__fig, master = self.__win)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side = tk.BOTTOM, 
+                                    fill = tk.BOTH, 
+                                    expand = True)
 
     def __create_window(self):
         self.__win.title('Smart Bridge Control Interface')
@@ -23,30 +50,31 @@ class SmartBridgeGui:
 
         self.__win.geometry(width + "x" + height)
 
-        self.__show_plot();
+    # def __handle_arduino_data(self, data: str):
+    #     print(data)
+    #     if data.startswith("W:"):
+    #         wlv = data.split(":")[1]
+    #         #self.__plot.add_data(float(wlv))
 
-    def __handle_arduino_data(self, data: str):
-        print(data)
-        if data.startswith("W:"):
-            wlv = data.split(":")
-            self.__plot.add_data(wlv[1])
-
-    def __arduino_read(self):
-        print("Thread started")
-        while True:
-            str = self.__uno.read()
-            if (str == ""): return
-            self.__handle_arduino_data(str)
+    # def __arduino_read(self):
+    #     print("Thread started")
+    #     while True:
+    #         str = self.__uno.read()
+    #         if (str == ""): return
+    #         self.__handle_arduino_data(str)
 
     def __init__(self):
         self.__create_window()
         print("Win created")
+        self.__add_plotter()
 
-        t = th.Thread(target=self.__arduino_read)
-        t.start()
+        self.__anm = animation.FuncAnimation(self.__fig, self.__animate, interval=100)
 
-        while (t.is_alive() == False):
-            time.sleep(1)
+        # t = th.Thread(target=self.__arduino_read)
+        # t.start()
+
+        # while (t.is_alive() == False):
+        #     time.sleep(1)
 
         print("Thread started?")
         print("Starting mainloop...")
